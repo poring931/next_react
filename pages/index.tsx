6c730e7e5d9ce5b9ffe8847abc67008824d80956
poring931 <poring.m@mail.ru>
2022-09-06 22:47:47 +0300

    add rating component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HTag from '../components/Htag/Htag';
 import PTag from '../components/PTag/PTag';
 import Tag from '../components/Tag/Tag';
 import Button from '../components/Button/Button';
+import Rating from '../components/Rating/Rating';
 
 
 export default function Home(): JSX.Element {
+
+  const [rating, setRating] = useState<number>(4);
+
 
   return (
     <div>
@@ -16,6 +20,7 @@ export default function Home(): JSX.Element {
         <Tag color='red'>asdassad</Tag>
         <Tag color='red' size='m'>asdassad</Tag>
         <Tag color='red' size='m' href='/sadsadsa'>asdassad</Tag>
+        <Rating rating={rating} isEditable setRating={setRating} />
     </div>
   );
 }
